8a2f086bb7400e349c241668a4ad9d97f3967ea9
Quang Minh Pham <admin@quangminhdigital.com>
2025-07-27 20:25:14 +0700

    architechture Behavioral
    Creat module AND1 ; NOT1 ; OR1 ; XOR1
    end Behavioral

---
+++ NOT1.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/14/2025 06:29:26 PM
+-- Design Name: 
+-- Module Name: NOT1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity NOT1 is
+    Port ( A : in STD_LOGIC;
+           Y : out STD_LOGIC);
+end NOT1;
+
+architecture Behavioral of NOT1 is
+
+begin
+Y <= NOT A;
+
+end Behavioral;
---
+++ OR1.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/14/2025 06:29:26 PM
+-- Design Name: 
+-- Module Name: OR1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity OR1 is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           Y : out STD_LOGIC);
+end OR1;
+
+architecture Behavioral of OR1 is
+
+begin
+Y <= A OR B;
+
+end Behavioral;
